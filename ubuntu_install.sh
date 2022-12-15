ff3cb73df044a0e6a90afde123da0be0c483f3de
Philip Sjöberg <phdah.sjoberg@gmail.com>
2022-12-15 22:56:35 +0100

    initial commit

---
+++ ubuntu_install.sh
@@ -0,0 +1,19 @@
+# Install Google Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
+# Install packages
+apt upgrade --yes
+apt update --yes
+
+apt install --yes \
+	i3 \
+	zsh \
+	git \
+	arandr \
+	curl \
+	neovim
+
+apt update --yes
+	
