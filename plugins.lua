3ba36d62dcab8937ed61c64588c136aa67ad2f01
Philip <phdah.sjoberg@gmail.com>
2023-04-04 11:04:57 +0200

    Add nvim line comment toggle

--- plugins.lua
+++ plugins.lua
@@ -8,15 +8,31 @@ return require('packer').startup(function()
 
     -- You add plugins here
     use {
-        'junegunn/fzf', run = ":call fzf#install()"
+        'junegunn/fzf',
+        run = ":call fzf#install()"
     }
-    use { 'junegunn/fzf.vim' }
+    use {
+        'junegunn/fzf.vim'
+    }
 
     use {
-      'unblevable/quick-scope'
+        'unblevable/quick-scope'
     }
 
     use {
-      'hashivim/vim-terraform'
+        'hashivim/vim-terraform'
+    }
+    use {
+        "terrortylor/nvim-comment",
+        require('nvim_comment').setup(
+            {
+                -- should comment out empty or whitespace only lines
+                comment_empty = false,
+                -- Normal mode mapping left hand side
+                line_mapping = "gcc",
+                -- Visual/Operator mapping left hand side
+                operator_mapping = "gc"
+            }
+        )
     }
 end)
