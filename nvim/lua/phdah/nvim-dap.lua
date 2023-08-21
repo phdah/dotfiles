60b7ce1e8c94c916fad20437475a05c9a4f08c33
phdah <philip.dahlqvist-sjoeberg@sbab.se>
2023-08-21 17:00:53 +0200

    Setup dap, lsp, some remaps, some install fixes

--- nvim/lua/phdah/nvim-dap.lua
+++ nvim/lua/phdah/nvim-dap.lua
@@ -1,12 +1,110 @@
+local dap_ok, dap = pcall(require, "dap")
+if not (dap_ok) then
+  print("nvim-dap not installed!")
+  return
+end
+
+require('dap').set_log_level('DEBUG') -- Helps when configuring DAP, see logs with :DapShowLog
+
+-- Mason setup
+require("mason").setup()
+require("mason-nvim-dap").setup({
+    ensure_installed = { "codelldb", "bash-debug-adapter" }
+})
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
+    args = {"--port", "${port}"},
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
+
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "codelldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+
+
 -- Configure dap-python
-require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
+require('dap-python').setup('/usr/bin/python3')
+
+-- Configure dap bash
+dap.adapters.bashdb = {
+  type = 'executable';
+  command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter';
+  name = 'bashdb';
+}
+
+dap.configurations.sh = {
+  {
+    type = 'bashdb';
+    request = 'launch';
+    name = "Launch file";
+    showDebugOutput = true;
+    pathBashdb = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir/bashdb';
+    pathBashdbLib = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/extension/bashdb_dir';
+    trace = true;
+    file = "${file}";
+    program = "${file}";
+    cwd = '${workspaceFolder}';
+    pathCat = "cat";
+    pathBash = "/bin/bash";
+    pathMkfifo = "mkfifo";
+    pathPkill = "pkill";
+    args = {};
+    env = {};
+    terminalKind = "integrated";
+  }
+}
+
+-- Configure dap lua
+dap.configurations.lua = {
+  {
+    type = 'nlua',
+    request = 'attach',
+    name = "Attach to running Neovim instance",
+  }
+}
+
+dap.adapters.nlua = function(callback, config)
+  callback({ type = 'server',
+    host = config.host or "127.0.0.1",
+    port = config.port or 8086
+})
+end
+
+_G.MyDapContinue = function()
+  local ft = vim.bo.filetype  -- Get current file type
+  if ft == "lua" then
+    require"osv".run_this()
+  else
+    require"dap".continue()
+  end
+end
 
 -- Configure dapui
 require("dapui").setup({
-  icons = { expanded = "", collapsed = "", current_frame = "" },
+  icons = { expanded = "|", collapsed = ">", current_frame = "" },
   mappings = {
     -- Use a table to apply multiple mappings
-    expand = { "<CR>", "<2-LeftMouse>" },
+    expand = { "<C-j>" },
     open = "o",
     remove = "d",
     edit = "e",
@@ -34,8 +132,7 @@ require("dapui").setup({
   layouts = {
     {
       elements = {
-      -- Elements can be strings or table with id and size keys.
-        { id = "scopes", size = 0.25 },
+        { id = "scopes", size = 0.4 },
         "breakpoints",
         "stacks",
         "watches",
@@ -43,66 +140,59 @@ require("dapui").setup({
       size = 40, -- 40 columns
       position = "left",
     },
-    {
-      elements = {
-        -- "console",
-        "repl"
-        -- {id = "repl", size = .75 },
-      },
-      size = 0.25, -- 25% of total lines
-      position = "bottom",
-    },
   },
-  controls = {
-    -- Requires Neovim nightly (or 0.8 when released)
-    enabled = true,
-    -- Display controls in this element
-    element = "repl",
-    icons = {
-      pause = "",
-      play = "",
-      step_into = "",
-      step_over = "",
-      step_out = "",
-      step_back = "",
-      run_last = "",
-      terminate = "",
-    },
-  },
+  -- controls = {
+  --   enabled = true,
+  --   element = "repl",
+  --   icons = {
+  --     pause = "",
+  --     play = "",
+  --     step_into = "",
+  --     step_over = "",
+  --     step_out = "",
+  --     step_back = "",
+  --     run_last = "",
+  --     terminate = "",
+  --   },
+  -- },
   floating = {
-    max_height = nil, -- These can be integers or a float between 0 and 1.
-    max_width = nil, -- Floats will be treated as percentage of your screen.
-    border = "single", -- Border style. Can be "single", "double" or "rounded"
+    max_height = nil,
+    max_width = nil,
+    border = "single",
     mappings = {
       close = { "q", "<Esc>" },
     },
   },
   windows = { indent = 1 },
   render = {
-    max_type_length = nil, -- Can be integer or nil.
-    max_value_lines = 100, -- Can be integer or nil.
+    max_type_length = nil,
+    max_value_lines = 100,
   }
 })
 
-local function close_nvim_ide_panels()
-    if pcall(require, 'ide') then
-        local ws = require('ide.workspaces.workspace_registry').get_workspace(vim.api.nvim_get_current_tabpage())
-        if ws ~= nil then
-            ws.close_panel(require('ide.panels.panel').PANEL_POS_BOTTOM)
-            ws.close_panel(require('ide.panels.panel').PANEL_POS_LEFT)
-            ws.close_panel(require('ide.panels.panel').PANEL_POS_RIGHT)
-        end
-    end
+-- Save your custom status line
+local custom_statusline = vim.o.statusline
+
+-- Function to set the status line to only display the file type
+local function set_dap_statusline()
+    vim.o.statusline = '%y'
 end
 
-local dap, dapui = require("dap"), require("dapui")
+-- Function to restore your custom status line
+local function restore_statusline()
+    vim.o.statusline = custom_statusline
+end
+
+-- Setup event listener to start dapui
 dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open({})
-    close_nvim_ide_panels()
+    require("dapui").open({})
+    set_dap_statusline()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-    dapui.close({})
+
+-- Setup close function for dapui
+_G.Dapui_terminate = function()
+    require("dap").terminate()
+    require("dapui").close()
+    restore_statusline()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
-    dapui.close({})
-end
+
