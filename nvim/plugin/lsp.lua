e55d2e2f65d75a4e45e02d470c2f6ef52cc856ad
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-03-30 11:49:52 +0100

    Add specific python version to LSP

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -34,6 +34,23 @@ vim.diagnostic.config({
     signs = false,
 })
 
+-------------------
+-- Python config --
+-------------------
+
+require("lspconfig").pyright.setup{
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true
+            },
+            pythonPath = "/opt/homebrew/bin/python3.10"
+        }
+    }
+}
+
 ----------------
 -- Metals lsp --
 ----------------
@@ -43,6 +60,7 @@ local metals_config = require("metals").bare_config()
 -- Build in automatic setup dap adapter
 metals_config.on_attach = function(client, bufnr)
     require("metals").setup_dap()
+    vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', { noremap = true, silent = true })
 end
 
 
