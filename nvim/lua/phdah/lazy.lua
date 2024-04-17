2d152751164948f72574dec19d16121f4475bd3f
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-04-17 12:29:35 +0200

    Add flash nvim plugin

--- nvim/lua/phdah/lazy.lua
+++ nvim/lua/phdah/lazy.lua
@@ -89,12 +89,13 @@ return require('lazy').setup({
         }
       },
     {
-      "folke/flash.nvim",
-      event = "VeryLazy",
-      keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      },
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+        },
     },
 
     {'shaunsingh/nord.nvim'},
