eb65eeb214c0d85fcb80481beec2b8d278fd9ad6
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-07-08 22:40:37 +0200

    Fix dbee cmp

--- nvim/lua/phdah/nvim-dbee.lua
+++ nvim/lua/phdah/nvim-dbee.lua
@@ -16,13 +16,4 @@ require("dbee").setup({
         }
     }
 })
-local auGroup = vim.api.nvim_create_augroup("nvim-dbee-custom", {clear = true})
-vim.api.nvim_create_autocmd("FileType", {
-    group = auGroup,
-    pattern = "sql",
-    callback = function()
-        vim.opt_local.commentstring = "-- %s"
-        -- Seems to not work for some reason...
-        vim.cmd("TSEnable highlight")
-    end
-})
+
