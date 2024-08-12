6b5ab7c2c52e8d688b61b470727f60f62c999d49
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-08-12 15:59:47 +0200

    Remove java from metals

--- nvim/lua/phdah/lsp.lua
+++ nvim/lua/phdah/lsp.lua
@@ -69,7 +69,7 @@ metals_config.on_attach =
 local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals",
                                                       {clear = true})
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {"scala", "sbt", "java"},
+    pattern = {"scala", "sbt"},
     callback = function()
         require("metals").initialize_or_attach(metals_config)
     end,
