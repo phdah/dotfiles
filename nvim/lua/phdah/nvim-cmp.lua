a7e5f6be35ec8c40a300127c4077ac7a2657d8c3
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-11-20 14:27:17 +0100

    Deprecation of buffer function
    https://neovim.io/doc/user/deprecated.html#nvim_buf_get_option()

--- nvim/lua/phdah/nvim-cmp.lua
+++ nvim/lua/phdah/nvim-cmp.lua
@@ -145,7 +145,7 @@ end, {})
 
 require("cmp").setup({
     enabled = function()
-        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or
+        return vim.api.nvim_get_option_value("buftype", { buf = 0 }) ~= "prompt" or
                    require("cmp_dap").is_dap_buffer()
     end
 })
