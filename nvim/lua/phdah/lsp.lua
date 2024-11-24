cbb8aa71c37729b4eede543daaba47153590dde2
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-11-24 14:50:20 +0100

    Disable ruff for hover

--- nvim/lua/phdah/lsp.lua
+++ nvim/lua/phdah/lsp.lua
@@ -32,6 +32,20 @@ require('mason-lspconfig').setup_handlers({
 vim.diagnostic.config({virtual_text = true, signs = false})
 
 -- TODO: Figure out how to use ruff as the LSP instead
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover',
+                                        {clear = true}),
+    callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client == nil then return end
+        if client.name == 'ruff' then
+            -- Disable hover in favor of Pyright
+            client.server_capabilities.hoverProvider = false
+        end
+    end,
+    desc = 'LSP: Disable hover capability from Ruff (https://docs.astral.sh/ruff/editors/setup/#neovim)'
+})
+
 lspconfig.pyright.setup {
     settings = {
         python = {
