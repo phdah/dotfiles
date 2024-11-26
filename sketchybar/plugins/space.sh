cf149d176c9024e7da44142bcc99d1b040e76591
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-11-26 13:25:25 +0100

    Only show sketchy spaces if not empty

--- sketchybar/plugins/space.sh
+++ sketchybar/plugins/space.sh
@@ -4,4 +4,26 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 
-sketchybar --set "$NAME" background.drawing="$SELECTED"
+# Get the space ID from the component name (e.g., space.1 -> 1)
+SPACE_ID=$(echo "$NAME" | cut -d'.' -f2)
+
+# Query the space details
+SPACE_DETAILS=$(yabai -m query --spaces --space "$SPACE_ID")
+
+# Check if the space is focused (boolen)
+HAS_FOCUS=$(echo "$SPACE_DETAILS" | jq '.["has-focus"]')
+
+# Check if the space has windows (int)
+WINDOW_COUNT=$(echo "$SPACE_DETAILS" | jq '.windows | length')
+
+# Determine visibility
+if [ "$HAS_FOCUS" = "true" ]; then
+  # Always show the focused space
+  sketchybar --set "$NAME" drawing=on background.drawing="$SELECTED"
+elif [ "$WINDOW_COUNT" -eq 0 ]; then
+  # Hide the space if it is empty and not focused
+  sketchybar --set "$NAME" drawing=off
+else
+  # Show the space if it is not empty
+  sketchybar --set "$NAME" background.drawing="$SELECTED"
+fi
