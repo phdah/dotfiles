b3f729a04557a188e0c1845c54c08731f2eb1995
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-01-27 09:05:45 +0100

    Add window resize and formatting

--- nvim/lua/phdah/settings.lua
+++ nvim/lua/phdah/settings.lua
@@ -5,17 +5,17 @@
 vim.opt.termguicolors = true
 
 -- Setup copy/paste for Windows
-vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
+vim.o.clipboard = vim.o.clipboard .. "unnamedplus"
 vim.g.clipboard = {
-    name = 'pbcopy/paste',
-    copy = {['+'] = 'pbcopy', ['*'] = 'pbcopy'},
-    paste = {['+'] = 'pbpaste', ['*'] = 'pbpaste'},
-    cache_enabled = 0
+    name = "pbcopy/paste",
+    copy = { ["+"] = "pbcopy", ["*"] = "pbcopy" },
+    paste = { ["+"] = "pbpaste", ["*"] = "pbpaste" },
+    cache_enabled = 0,
 }
 
 -- Width limit
 vim.wo.colorcolumn = "80"
-vim.cmd('highlight ColorColumn guifg=#4C566A')
+vim.cmd("highlight ColorColumn guifg=#4C566A")
 
 -- Set up default tabs
 local setDefaults = function()
@@ -30,21 +30,25 @@ local setDefaults = function()
         vim.o.expandtab = true -- use spaces instead of tab
     end
 
+    -- Auto resize windows when window changes size
+    vim.api.nvim_create_autocmd("VimResized", {
+        command = "wincmd =",
+    })
+
     -- Auto-indent configurations
     vim.o.autoindent = false -- Disable auto-indentation
     vim.bo.autoindent = false -- Ensure local buffer autoindent is off
     vim.cmd("filetype indent off") -- Disable filetype-specific indentation
     -- Adjust formatoptions
-    vim.opt.formatoptions:remove({"c", "r", "o"}) -- Remove auto-commenting and auto-wrapping
+    vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Remove auto-commenting and auto-wrapping
 end
 setDefaults()
 
-local defaultGroup = vim.api.nvim_create_augroup("DefaultTabSettings",
-                                                 {clear = true})
-vim.api.nvim_create_autocmd({"BufEnter"}, {
+local defaultGroup = vim.api.nvim_create_augroup("DefaultTabSettings", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
     group = defaultGroup,
     pattern = "*",
-    callback = setDefaults
+    callback = setDefaults,
 })
 
 -- Set automatic pwd to the current buffer's pwd
@@ -54,33 +58,34 @@ vim.o.autochdir = true
 vim.opt.swapfile = false
 
 -- Folding
-vim.wo.foldmethod = 'indent'
+vim.wo.foldmethod = "indent"
 vim.wo.foldenable = false
 
 -- Set to only one status line
 vim.o.laststatus = 3
 
 -- Highlighting for white space
-local whiteGroup = vim.api.nvim_create_augroup("MyCustomWhiteSpaceHighlights",
-                                               {clear = true})
-vim.api.nvim_create_autocmd({"BufEnter", "ColorScheme"}, {
+local whiteGroup = vim.api.nvim_create_augroup("MyCustomWhiteSpaceHighlights", { clear = true })
+vim.api.nvim_create_autocmd({ "BufEnter", "ColorScheme" }, {
     group = whiteGroup,
     pattern = "*",
     callback = function()
         local fileType = vim.bo.filetype
         if fileType ~= "" and fileType ~= "dbee" then
             -- Only apply the highlight if the buffer has a filetype
-            vim.api.nvim_set_hl(0, "ExtraWhitespace", {bg = "#BF616A"})
+            vim.api.nvim_set_hl(0, "ExtraWhitespace", { bg = "#BF616A" })
             vim.fn.matchadd("ExtraWhitespace", "\\s\\+$")
         end
-    end
+    end,
 })
 
 -- Clean out all trailing ExtraWhitespace, and tabs
-vim.api.nvim_create_user_command('Clean', function()
+vim.api.nvim_create_user_command("Clean", function()
     local snacks = require("snacks")
     -- Clean out all trailing ExtraWhitespace, and tabs
-    local ok, _ = pcall(function() vim.cmd("%s/\\t\\+$\\| \\+$//") end)
+    local ok, _ = pcall(function()
+        vim.cmd("%s/\\t\\+$\\| \\+$//")
+    end)
     if ok then
         snacks.notify.info("Found and removed all trailing whitespace")
     end
@@ -92,39 +97,39 @@ vim.wo.relativenumber = true
 
 -- Disable mouse
 require("nvim-utils").Mouse:new(false)
-vim.api.nvim_create_user_command('MouseToggle', function()
+vim.api.nvim_create_user_command("MouseToggle", function()
     require("nvim-utils").Mouse:toggle()
 end, {})
 
 -- Open help in its own buffer, use ':H <args>'
-vim.cmd [[command! -nargs=1 -complete=command -bar H help <args> | only]]
+vim.cmd([[command! -nargs=1 -complete=command -bar H help <args> | only]])
 
 -- Center search
 vim.o.scrolloff = 0
 
 -- Set colorscheme to nord
-vim.cmd [[colorscheme nord]]
+vim.cmd([[colorscheme nord]])
 
 -- Set filechar and colors for diff
 vim.opt.fillchars:append("diff: ")
 -- Tokyonight colors: https://github.com/folke/tokyonight.nvim/blob/775f82f08a3d1fb55a37fc6d3a4ab10cd7ed8a10/extras/lua/tokyonight_night.lua#L899
-vim.api.nvim_set_hl(0, "DiffAdd", {bg = "#20303b"})
-vim.api.nvim_set_hl(0, "DiffDelete", {bg = "#37222c"})
-vim.api.nvim_set_hl(0, "DiffChange", {bg = "#1f2231"})
-vim.api.nvim_set_hl(0, "DiffText", {bg = "#394b70"})
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#20303b" })
+vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#37222c" })
+vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1f2231" })
+vim.api.nvim_set_hl(0, "DiffText", { bg = "#394b70" })
 
 -- Set spelling on for specific files
-local auGroupSpelling = vim.api.nvim_create_augroup("nvim-spelling-custom",
-                                                    {clear = true})
+local auGroupSpelling = vim.api.nvim_create_augroup("nvim-spelling-custom", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
     group = auGroupSpelling,
     pattern = "markdown",
-    callback = function() vim.cmd("setlocal spell! spelllang=en_us") end
+    callback = function()
+        vim.cmd("setlocal spell! spelllang=en_us")
+    end,
 })
 
 -- Set configs for floaterm
-local auGroupFloaterm = vim.api.nvim_create_augroup("nvim-floaterm-custom",
-                                                    {clear = true})
+local auGroupFloaterm = vim.api.nvim_create_augroup("nvim-floaterm-custom", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
     group = auGroupFloaterm,
     pattern = "floaterm",
@@ -135,46 +140,46 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.api.nvim_create_autocmd("BufLeave", {
             buffer = 0,
             once = true,
-            callback = function() vim.o.mouse = "" end
+            callback = function()
+                vim.o.mouse = ""
+            end,
         })
-    end
+    end,
 })
 
 -- Function to set the executor based on file type
 local function defineMake(args)
     local filetype = vim.bo.filetype
-    if filetype == 'python' then
-        vim.cmd('DBRun ' .. args) -- nvim-databricks run command
-    elseif filetype == 'sh' then
-        vim.cmd('!bash % ' .. args)
-    elseif filetype == 'c' or filetype == 'cpp' then
-        vim.cmd('!make -C build && ./build/%< ' .. args)
-    elseif filetype == 'scala' then
+    if filetype == "python" then
+        vim.cmd("DBRun " .. args) -- nvim-databricks run command
+    elseif filetype == "sh" then
+        vim.cmd("!bash % " .. args)
+    elseif filetype == "c" or filetype == "cpp" then
+        vim.cmd("!make -C build && ./build/%< " .. args)
+    elseif filetype == "scala" then
         vim.cmd("!scala % " .. args)
-    elseif filetype == 'haskell' then
-        vim.cmd("!ghc -no-keep-hi-files -no-keep-o-files -o %:r % && ./%:r " ..
-                    args)
-    elseif filetype == 'lua' then
+    elseif filetype == "haskell" then
+        vim.cmd("!ghc -no-keep-hi-files -no-keep-o-files -o %:r % && ./%:r " .. args)
+    elseif filetype == "lua" then
         vim.cmd("!luajit % " .. args)
-    elseif filetype == 'make' then
+    elseif filetype == "make" then
         vim.cmd("!make " .. args)
-    elseif filetype == 'go' then
+    elseif filetype == "go" then
         vim.cmd("!go run % " .. args)
     end
 end
 
 -- Create the :Make command
-vim.api.nvim_create_user_command('Make',
-                                 function(opts) defineMake(opts.args) end,
-                                 {nargs = "*"})
+vim.api.nvim_create_user_command("Make", function(opts)
+    defineMake(opts.args)
+end, { nargs = "*" })
 
 local function resizeSplitsEqually()
     local total_width = 0
-    local num_splits = vim.fn.winnr('$')
+    local num_splits = vim.fn.winnr("$")
     -- Iterate over all windows to get the total width
     for i = 1, num_splits do
-        total_width = total_width +
-                          vim.api.nvim_win_get_width(vim.fn.win_getid(i))
+        total_width = total_width + vim.api.nvim_win_get_width(vim.fn.win_getid(i))
     end
     local new_width = math.floor(total_width / num_splits)
     for i = 1, num_splits do
@@ -183,5 +188,6 @@ local function resizeSplitsEqually()
     end
 end
 
-vim.api.nvim_create_user_command('EqualizeSplits',
-                                 function() resizeSplitsEqually() end, {})
+vim.api.nvim_create_user_command("EqualizeSplits", function()
+    resizeSplitsEqually()
+end, {})
