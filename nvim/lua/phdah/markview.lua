4c6349646ae1daf33a351e95f3647b9532432cca
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-02-11 17:19:18 +0100

    Fix markview

--- nvim/lua/phdah/markview.lua
+++ nvim/lua/phdah/markview.lua
@@ -1,10 +1,8 @@
-require("markview").setup(
--- {
---     preview = {
---         enable = true,
---         filetypes = { "md", "rmd", "quarto", "octo" },
---         modes = { "n", "no", "c", "i" },
---         hybrid_modes = {"n", "i"},
---     },
--- }
-)
+require("markview").setup({
+    preview = {
+        enable = true,
+        filetypes = { "md", "rmd", "quarto", "markdown", "octo" },
+        modes = { "n", "no", "c", "i" },
+        hybrid_modes = { "n", "i" },
+    },
+})
