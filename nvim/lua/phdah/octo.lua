366277221f7b88df439921b35367e5ac66b7784f
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-03-04 09:16:15 +0100

    Set snacks as octo picker

--- nvim/lua/phdah/octo.lua
+++ nvim/lua/phdah/octo.lua
@@ -1,6 +1,7 @@
 require("octo").setup({
     suppress_missing_scope = { projects_v2 = true },
     enable_builtin = true,
+    -- picker = "snacks",
     mappings = {
         issue = {
             reload = { lhs = "<localleader>r", desc = "reload PR" },
