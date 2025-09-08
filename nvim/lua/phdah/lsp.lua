565243edefa0853e8f51319479c9df6317f54cd9
Philip Sjöberg <philip.sjoberg@schibsted.com>
2025-09-08 13:32:37 +0200

    Fix clang format

--- nvim/lua/phdah/lsp.lua
+++ nvim/lua/phdah/lsp.lua
@@ -242,7 +242,7 @@ local function lintFile(args)
         or filetype == "json"
         or filetype == "java"
     then
-        vim.cmd("silent! !" .. masonBinPath .. "clang-format -i % " .. args)
+        vim.cmd("silent! !clang-format -i % " .. args)
     elseif filetype == "lua" then
         vim.cmd(
             "silent! !"
