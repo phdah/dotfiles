549680b56f492fb7c88ec6c8955218ac27f62371
Philip Sjöberg <philip.sjoberg@vend.com>
2025-12-30 15:33:21 +0100

    Set current workspace after refresh i3

--- scripts/i3_workspaces.sh
+++ scripts/i3_workspaces.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/sh
+# Get current workspace, to restore it later.
+current_ws=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
+
+# Get primary screen
 primary=$(xrandr --query | awk '/ primary/{print $1}')
-for i in $(seq 9 -1 1); do
+for i in $(seq 9); do
     i3-msg "workspace $i; move workspace to output $primary"
 done
+
+i3-msg "workspace number $current_ws"
